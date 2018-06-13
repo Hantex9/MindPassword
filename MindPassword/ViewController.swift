31e0187b00c4a6184f50c83b532a9080420f4cb0
Alessandro Izzo <alessandro.izzo4@studenti.unina.it>
2018-06-13 14:57:37 +0200

    Initial commit.

---
+++ MindPassword/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MindPassword
+//
+//  Created by Alessandro Izzo on 13/06/18.
+//  Copyright © 2018 Alessandro Izzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
