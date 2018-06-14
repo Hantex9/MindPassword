0f0f1a9d309d9f65fac2815420108031ad0fae52
Alessandro Izzo <alessandro.izzo4@studenti.unina.it>
2018-06-14 22:17:25 +0200

    Inserted the Scroll View and adapted the layout on it.

--- MindPassword/Controllers/Registration/RegisterPasswordViewController.swift
+++ MindPassword/Controllers/Registration/RegisterPasswordViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class RegisterPasswordViewController: UIViewController {
   
+  @IBOutlet weak var scrollView: UIScrollView!
   @IBOutlet weak var passwordTextField: PasswordTextField!
   @IBOutlet weak var confirmPasswordTextField: PasswordTextField!
   @IBOutlet weak var confirmTopConstraint: NSLayoutConstraint!
@@ -20,6 +21,10 @@ class RegisterPasswordViewController: UIViewController {
     // Do any additional setup after loading the view.
     let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapHandler))
     view.addGestureRecognizer(tapGesture)
+    
+    //Notification for the Keyboard
+    NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+    NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
   }
   
   override func viewWillAppear(_ animated: Bool) {
@@ -28,12 +33,32 @@ class RegisterPasswordViewController: UIViewController {
     navigationController?.setNavigationBarHidden(false, animated: true)
   }
   
+  @objc fileprivate func keyboardWillShow(notification: NSNotification) {
+    if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+      if scrollView.frame.origin.y <= 0 {
+        scrollView.contentInset = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardSize.height, right: 0.0)
+        scrollView.scrollIndicatorInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardSize.height, right: 0.0)
+//        scrollView.frame.origin.y -= keyboardSize.height
+      }
+    }
+  }
+  
+  @objc fileprivate func keyboardWillHide(notification: NSNotification) {
+    if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+        scrollView.contentInset = UIEdgeInsets.zero
+        scrollView.scrollIndicatorInsets = UIEdgeInsets.zero
+//        scrollView.frame.origin.y += keyboardSize.height
+    }
+  }
+  
   // MARK: Actions
   @objc fileprivate func tapHandler(_ sender: UITapGestureRecognizer) {
     view.endEditing(true)
   }
   
   @IBAction func setPasswordButtonPressed(_ sender: UIButton) {
+    
+    view.endEditing(true)
     
     let errorMessages = DataManager.shared.isValidPassword(password: passwordTextField.text, confirmPassword: confirmPasswordTextField.text)
     
@@ -52,8 +77,9 @@ class RegisterPasswordViewController: UIViewController {
     }
     
     
-    confirmTopConstraint.constant = (passwordTextField.errorMessageLabel.text!.isEmpty) ? 15.0 : passwordTextField.errorMessageLabel.frame.size.height + 15.0
-    confirmTopConstraint.constant = (passwordTextField.errorMessageLabel.text!.isEmpty) ? 15.0 : passwordTextField.errorMessageLabel.frame.size.height + 15.0
+    if let error = passwordTextField.errorMessageLabel.text {
+      confirmTopConstraint.constant = (error.isEmpty) ? 38.0 : passwordTextField.errorMessageLabel.frame.size.height + 38.0
+    }
 
     
     if errorMessages.confirmPasswordError.isEmpty && errorMessages.passwordError.isEmpty {
